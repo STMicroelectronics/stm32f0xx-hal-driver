5b11ba1735ad57d44afde346bacf15214a0b7b59
Eya <aya.selmi1@st.com>
2023-01-09 10:47:03 +0100

    [HAL][UART] Disable the Receiver Timeout Interrupt when data reception is completed

--- Src/stm32f0xx_hal_uart.c
+++ Src/stm32f0xx_hal_uart.c
@@ -4002,6 +4002,13 @@ static void UART_RxISR_8BIT(UART_HandleTypeDef *huart)
       /* Clear RxISR function pointer */
       huart->RxISR = NULL;
 
+      /* Check that USART RTOEN bit is set */
+      if (READ_BIT(huart->Instance->CR2, USART_CR2_RTOEN) != 0U)
+      {
+        /* Enable the UART Receiver Timeout Interrupt */
+        ATOMIC_CLEAR_BIT(huart->Instance->CR1, USART_CR1_RTOIE);
+      }
+
       /* Check current reception Mode :
          If Reception till IDLE event has been selected : */
       if (huart->ReceptionType == HAL_UART_RECEPTION_TOIDLE)
@@ -4080,6 +4087,13 @@ static void UART_RxISR_16BIT(UART_HandleTypeDef *huart)
 
       /* Clear RxISR function pointer */
       huart->RxISR = NULL;
+
+      /* Check that USART RTOEN bit is set */
+      if (READ_BIT(huart->Instance->CR2, USART_CR2_RTOEN) != 0U)
+      {
+        /* Enable the UART Receiver Timeout Interrupt */
+        ATOMIC_CLEAR_BIT(huart->Instance->CR1, USART_CR1_RTOIE);
+      }
 
       /* Check current reception Mode :
          If Reception till IDLE event has been selected : */
